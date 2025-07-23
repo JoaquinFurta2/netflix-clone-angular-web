a564e6600c533109c4851c04d31ba7afcee10aaf
Furta <joaquinfurta@gmail.com>
2025-07-23 12:52:00 -0300

    changed how the slider wroks in order to stop it from flickering on slow devices

--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, computed, input  } from '@angular/core';
+import { Component, inject, signal, computed, input, viewChild, ElementRef, ViewChild, effect, linkedSignal  } from '@angular/core';
 
 import { SliderItemComponent } from '../slider-elem/slider-elem.component';
 import { DataService} from '../../services/data.service';
@@ -13,11 +13,14 @@ import { Movie, TVShow } from '../../models/dataService.model';
 })
 export class SliderComponent {
 
+   // Service Injection
+  dataService = inject(DataService)
+  resizeService = inject(ResizeService)
+
+  elem = viewChild.required('elem', {read:ElementRef})
   type = input.required< 'movie' | 'tv'>()
   data = input.required<Movie[] | TVShow[]>()
 
-  
- 
   //Constants
   timeSlideTransition = 800
 
@@ -26,15 +29,20 @@ export class SliderComponent {
     return this.data()
   });
  
-  currSlide = signal(0)
+  currSlide = signal(1)
   buttonDisabled = signal(true)
-  transitionPrev = signal(false)
-  transitionNext = signal(false)
+  disableTransition = signal(false)
+
+  
 
   // Computed Properties
+
+  itemWidth = computed(() => this.resizeService.sliderWidth());
+
   splitMovies = computed(() => {
     switch (this.resizeService.imgSize()) {
       case "sm" : 
+        console.log('sm')
         return this.splitM(3)
       case "md" :
         return this.splitM(4)
@@ -43,46 +51,49 @@ export class SliderComponent {
     }
   })
 
-  lastElem = computed(() => this.splitMovies().length - 1)
-  
-  nextSlide = computed(() => 
-    this.currSlide() === this.lastElem()  ? 0 : this.currSlide() + 1
-  )
+  trackTransform = computed(() => {
+    const width = this.itemWidth();
+    return `translateX(-${this.currSlide() * width}px)`;
+  });
 
-  prevSlide = computed(() => 
-   this.currSlide() === 0 ? this.lastElem() : this.currSlide() - 1
-  )
-
-  notClickable = computed ( () => this.transitionPrev() || this.transitionNext())
-  
-
- // Service Injection
-  dataService = inject(DataService)
-  resizeService = inject(ResizeService)
 
 
   // Lifecycle
   constructor() {
-   
   }
 
-  //Public Methods 
-  scrollNext() {
-    this.transitionNext.set(true)
-    setTimeout(() => {
-      this.currSlide.update(val =>  val === this.lastElem() ? 0 : val + 1)
-      this.transitionNext.set(false)
-      this.buttonDisabled.set(false)
-    }, this.timeSlideTransition); 
+
+
+ 
+
+scrollNext() {
+  this.disableTransition.set(false); 
+  this.currSlide.set(this.currSlide() + 1);
+
+  // Wait for the animation to finish
+  setTimeout(() => {
+    if (this.currSlide() === this.splitMovies().length - 1) {
+      this.disableTransition.set(true); 
+      this.currSlide.set(1);         
+    }
+  }, 800);
 }
 
-  scrollPrev() {
-    this.transitionPrev.set(true)
-    setTimeout(() => {
-      this.currSlide.update( val => val === 0 ? this.lastElem() : val - 1) 
-      this.transitionPrev.set(false)
-    }, this.timeSlideTransition);     
-}
+scrollPrev() {
+ this.disableTransition.set(false); 
+  this.currSlide.set(this.currSlide() - 1);
+
+  // Wait for the animation to finish
+  setTimeout(() => {
+
+    if (this.currSlide() === 0) {
+      this.disableTransition.set(true); 
+      this.currSlide.set(4);            
+    }
+  }, 800);
+} 
+
+
 
  // Private
   private splitM(n:number) {
@@ -91,6 +102,13 @@ export class SliderComponent {
     for (let i = 0 ; i < length ; i += n) {
       arr.push(this.movies().slice(i, i + n))
     }
+    arr.push(arr[0])
+    arr.unshift(arr.at(-2)!)
+
     return arr
+  }
+
+  ngAfterViewInit() {
+    this.resizeService.resizeObserver.observe(this.elem().nativeElement);
   }
 }
